1dabe0845f6e1467d8d51e79832590747316fae4
Mike Newell <mike@iwearshorts.com>
2012-09-09 23:21:44 -0700

    added a bunch of stuff to the cheetos app, connected desktop to iphone and got it running on heroku server

--- cheetopult/heroku/public/js/app.js
+++ cheetopult/heroku/public/js/app.js
@@ -7,7 +7,7 @@
     $(window).load(function() {
         
         // set global accel value
-        var accel = 0, tilt = { alpha: 0, beta: 0, gamma: 0 };
+        var accel =  { x: 0, y: 0, z: 0 }, tilt = { alpha: 0, beta: 0, gamma: 0 };
         
         $('.cheeto').on('touchstart', function(evt) {
             evt.preventDefault();
@@ -31,24 +31,73 @@
         });
         
         $(window).on('devicemotion', function() {
-            accel = (event.acceleration.x * 100).toFixed(2);
-        });
-        
-        $(window).on('deviceorientation', function() {
-//            tilt = (event.alpha * 10).toFixed(2);
-            
-            tilt.alpha = event.alpha.toFixed(2);
-            tilt.beta = event.beta.toFixed(2);
-            tilt.gamma = event.gamma.toFixed(2);
-        });
-        
-        setInterval(function() {
-            // accelerometer doesnt give us much
-//            console.log('acceleration: ' + accel);
+            // going with z since it gives us a good range
 
-            // beta seems really interesting here...
-            console.log('alpha: ' + tilt.alpha + 'beta: ' + tilt.beta + 'gamma: ' + tilt.gamma);
-        }, 500);
+//            accel.x = event.acceleration.x.toFixed(2);
+//            accel.y = event.acceleration.y.toFixed(2);
+            accel.z = event.acceleration.z.toFixed(2);
+            
+            // forward is a negative acceleration so we are looking for negative values here
+            if(accel.z < -12 && accel.z > -17) {
+                $('#message').html('<p>Small: ' + Math.abs(accel.z) + '</p>');
+                socket.emit('accel', { z : accel.z });
+                $('#cheetopult').addClass('animatingSlow');
+                flyingCheeto(0.35);
+            } else if(accel.z < -17 && accel.z > -19) {
+                $('#message').html('<p>Just right!: ' + Math.abs(accel.z) + '</p>');
+                socket.emit('accel', { z : accel.z });
+                $('#cheetopult').addClass('animatingJustRight');
+                flyingCheeto(0.35);
+            } else if(accel.z < -19) {
+                $('#message').html('<p>Too Much!: ' + Math.abs(accel.z) + '</p>');
+                socket.emit('accel', { z : accel.z });
+                $('#cheetopult').addClass('animatingFast');
+                flyingCheeto(0.35);
+            }
+            
+        });
+        
+        $('#cheetopult').on('animationend webkitAnimationEnd', function(evt) {
+            $(this).removeClass();
+        });
+        
+        var flyingCheeto = function(seconds) {
+            
+            $('#bowlContainer').css({ 
+                'top' : '-275px',
+                'opacity' : 0,
+                '-webkit-transition' : 'top '+seconds+'s ease-in, opacity 0.5s linear'
+            });
+            
+            setTimeout(function() {
+                
+                $('#bowlContainer').css({
+                    'background' : 'none',
+                    'top' : '275px',
+                    'opacity' : 1,
+                    '-webkit-transition' : 'none'
+                });
+                
+            }, 1000);
+            
+        };
+        
+        // DEPRECATED - because we went with accel instead
+//        $(window).on('deviceorientation', function() {
+//            tilt.alpha = event.alpha.toFixed(2);
+//            tilt.beta = event.beta.toFixed(2);
+//            tilt.gamma = event.gamma.toFixed(2);
+//        });
+        
+        // DEPRECATED BECAUSE I DON'T NEED A TIMER - WE JUST SEND THE MESSAGE IF THE VALUE MATCHES WHAT WE'RE LOOKING FOR
+//        setInterval(function() {
+//        
+//            // beta seems really interesting here...but its only the angle of rotation (0 - 90 - 0)
+//            // need to do some sort of check to see if beta goes above a value, then send the socket
+////            socket.emit('gyro', { beta : tilt.beta });
+//            socket.emit('accel', { z : accel.z });
+//            console.log('alpha: ' + tilt.alpha + 'beta: ' + tilt.beta + 'gamma: ' + tilt.gamma);
+//        }, 500);
 
     });
 })(jQuery);
--- cheetopult/heroku/server.js
+++ cheetopult/heroku/server.js
@@ -3,14 +3,6 @@
  * and open the template in the editor.
  */
 
-//var App = function() {
-//    this.setHeartbeat = function(s) {
-//        var s = s;
-//        setInterval(function() {
-//            s.emit('hi', {connection: 'listening for heartbeat'});
-//        }, 2000);
-//    }
-//}
 
 var fs = require('fs'),
     express = require('express'),
@@ -30,6 +22,7 @@ io.configure(function () {
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
+    console.log(req);
     res.sendfile(__dirname + '/public/index.html');
 });
 
@@ -37,24 +30,23 @@ app.get('/iphone', function(req, res) {
     res.sendfile(__dirname + '/public/iphone.html');
 });
 
-//var a = new App();
-//io.sockets.on('connection', function(socket) {
-//    
-//    socket.emit('hi', { connection: 'established' });
-//    
-//    socket.on('hi back', function(data) {
+io.sockets.on('connection', function(socket) {
+    
+    socket.emit('hi', { connection: 'established' });
+    
+    socket.on('hi back', function(data) {
+        console.log(data);
+    });
+    
+    socket.on('gyro', function(data) {
 //        console.log(data);
-//    });
-//    
-//    socket.on('touchstart', function(data) {
-//        console.log(data);
-//        socket.broadcast.emit('touchstart', data);
-//    });
-//    
-//    socket.on('touchend', function(data) {
-//        console.log(data);
-//        socket.broadcast.emit('touchend', data);
-//    });
-//    
-//});
+        
+    });
+    
+    socket.on('accel', function(data) {
+        console.log(data);
+        socket.broadcast.emit('accel', data);
+    });
+    
+});
 
